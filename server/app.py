bcf4831529d4bff946f0ac73190c5dab901ef5f1
Dicky Darmawan <dicky93darmawan@gmail.com>
2024-06-11 19:59:17 +0800

    new key

--- server/app.py
+++ server/app.py
@@ -42,7 +42,7 @@ k_value = 5
 knn_model = KNeighborsClassifier(n_neighbors=k_value)
 knn_model.fit(x_train, y_train.argmax(axis=1))
 
-GOOGLE_API_KEY="AIzaSyAOiZU7wP_HrIJZdQ5wsSn-Kj59dvJji8Y"
+GOOGLE_API_KEY="AIzaSyBfD_j2jhR-w7isZ1EuNy_pBAwZYdVJBLI"
 
 genai.configure(api_key=GOOGLE_API_KEY)
 
